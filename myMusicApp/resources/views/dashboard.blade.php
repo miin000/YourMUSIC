4561ac5e07ea9eaf89d2e6afb35974fd46d08ea3
miin0 <phammin000@gmail.com>
2025-01-09 15:29:21 +0700

    update view

--- myMusicApp/resources/views/dashboard.blade.php
+++ myMusicApp/resources/views/dashboard.blade.php
@@ -3,6 +3,7 @@
         <p style="font-size: 48px; font-weight: bold; background: linear-gradient(to right, #800080, #FFD700, #00FF00, #0000FF); -webkit-background-clip: text; color: transparent;">
             Your<span style="background: linear-gradient(to right, #0000FF, #00FF00, #FFD700, #800080); -webkit-background-clip: text; color: transparent;">MUSIC</span>
         </p>
+        
     </x-slot>
 
     <div class="py-12">
