6f3d6a6fb40d25dbcd178271cbc881ca9fe3b8a5
miin0 <phammin000@gmail.com>
2025-01-09 13:46:10 +0700

    fix conflict

--- YourMusicApp/public/test.php
+++
@@ -1,4 +0,0 @@
-<?php
-echo "Upload Max Filesize: " . ini_get('upload_max_filesize') . "\n";
-echo "Post Max Size: " . ini_get('post_max_size') . "\n";
-echo "Memory Limit: " . ini_get('memory_limit') . "\n";
--- YourMusicApp/resources/views/components/application-mark.blade.php
+++
@@ -1,3 +0,0 @@
-<svg viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg" {{ $attributes }}>
-  <img src="{{ asset('images/logo.png') }}" alt="Logo" class="h-9 w-auto" style="margin-top: -25px; margin-left: -5px;" />
-</svg>
