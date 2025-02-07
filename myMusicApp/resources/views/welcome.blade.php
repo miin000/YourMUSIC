6594f008e5d148e5adf0b462e9184bf3598d2b2a
Phạm Quang Minh <phammin000@gmail.com>
2025-02-07 23:29:35 +0700

    Update welcome.blade.php

--- myMusicApp/resources/views/welcome.blade.php
+++ myMusicApp/resources/views/welcome.blade.php
@@ -39,7 +39,7 @@
                                         <p style="font-size: 48px; font-weight: bold; background: linear-gradient(to right, #800080, #FFD700, #00FF00, #0000FF); -webkit-background-clip: text; color: transparent;">
                                             Your<span style="background: linear-gradient(to right, #0000FF, #00FF00, #FFD700, #800080); -webkit-background-clip: text; color: transparent;">MUSIC</span>
                                         </p>
-                                        <p>Go to the home</p>
+                                        <p>Visit Home</p>
                                     </a>
                                 @else
                                     <a
